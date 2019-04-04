c5c5745c3fc6daa04cb6038032a70bd1b86c1da5
harunbas <harun.bas@medianova.com>
2019-04-04 15:39:53 +0300

    comments deleted.

--- binary_search.py
+++ binary_search.py
@@ -1,48 +1,23 @@
-sorted_list = list(range(200))
+sorted_list = list(range(1000))
 
-#Methodumuzu oluşturuken parametreleri ekliyoruz.Listemiz ve hedef sayı
 def binary_search(sorted_list, target):
-
-#Listeyi değişkene atadık.
+    
     search_area = sorted_list
-
-#Birden fazla sayı olması gerekiyor ki aramayı yapabilelim.
+    i = 0
     while len(search_area) >=1:
-
-#Elimizdeki listeyi gösteryiyoruz.
         print('Search area: ', search_area)
-
-#Son indisi çekiyoruz.
         end = len(search_area) - 1
-
-#İlk indisi çekiyoruz.
         middle = int((0+end)/2)
-
-#Hedef sayı, ortadaki sayıdan daha küçükse
+        i = i + 1
+        
         if target < search_area[middle]:
-
-#Hedef sayıyı, ortadaki sayıdan önce arıyoruz.
             search_area = search_area[:middle]
-
-#Hedef sayı, ortadaki sayıdan önce büyükse
         elif target > search_area[middle]:
-
-#Hedef sayıyı, ortadaki sayıdan sonra arıyor.
             search_area = search_area[middle:]
-
-#Eğer hedef sayı, ortadaki sayıya eşitse dögümüz bitiyor.
         else:
-
-
             return print("Found it", str(search_area[middle]))
-
-#Eğer 0 tane eleman varsa döngümüz çalışmaz.
-
-
     else:
-
-
         return print("Not in list")
 
 
-binary_search(sorted_list, 7)
+binary_search(sorted_list, 100)
